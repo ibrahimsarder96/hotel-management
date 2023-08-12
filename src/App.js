dca8a12465fc3cc0a2932d4ae458d580fac11b86
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-08-12 23:22:41 +0600

    background gradient

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
-
+import './App.css';
 
 function App() {
   return (
-    <div>
-     <Header></Header>
-     <Hero></Hero>
+    <div className="App">
+      <div>
+        <div className="white-gradient"/>
+        <Header></Header>
+        <Hero></Hero>
+      </div>
     </div>
   );
 }
